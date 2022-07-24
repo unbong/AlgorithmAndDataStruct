580b4dc09994e3cc2155f66edcc54ddc82d4341f
unbong <ecunbong@gmail.com>
2022-07-24 14:30:03 +0900

    第二章 第2节

---
+++ src/main/java/chapter2/section1/Merge.java
@@ -0,0 +1,67 @@
+package chapter2.section1;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Merge {
+    public static Comparable aux[];
+    public static void merge(Comparable a[], int lo, int mid, int hi )
+    {
+        int i = lo;
+        int j = mid +1;
+
+        //Comparable aux[] = new Comparable[a.length];
+        for (int k = i; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if(i>mid) { a[k] = aux[j++]; }
+            else if (j > hi) {  a[k] = aux[i++] ;}
+            else if (less(aux[j], aux[i])) { a[k] = aux[j++] ;}
+            else {a[k] = aux[i++];}
+
+        }
+    }
+
+    public static void sort(Comparable a[])
+    {
+        aux = new Comparable[a.length];
+        for (int i = 0; i < a.length; i++) {
+            aux[i] = a[i];
+        }
+
+        sort(a, 0, a.length-1);
+
+    }
+
+    private static void sort(Comparable a[], int lo, int hi)
+    {
+        if(hi<=lo) return;
+
+        int mid = lo+ (hi-lo)/2;
+        sort(a, lo, mid);
+        sort(a, mid+1, hi);
+
+        merge(a, lo, mid, hi);
+    }
+
+    public static boolean less(Comparable v, Comparable w)
+    {
+        return v.compareTo(w)<0;
+    }
+
+    public static void show(Comparable a[])
+    {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(" " + a[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Integer a[]={5,3, 2,1, 4};
+
+        sort(a);
+
+        show(a);
+    }
+}
---
+++ src/main/java/chapter2/section1/MergeBU.java
@@ -0,0 +1,59 @@
+package chapter2.section1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class MergeBU {
+
+    private static Comparable aux[] = null;
+
+    private static boolean less(Comparable v, Comparable w)
+    {
+        return v.compareTo(w)< 0;
+    }
+
+    public static void merge(Comparable a[], int lo, int mi, int hi)
+    {
+        aux = new Comparable[a.length];
+        int i = lo;
+        int j = mi+1;
+        for (int m = lo; m <= hi; m++) {
+            aux[m] = a[m];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if(i>mi) { a[k] = aux[j++]; }
+            else if (j > hi) { a[k] = aux[i++] ;}
+            else if(less(aux[j], aux[i])) { a[k] = aux[j++]; }
+            else { a[k] = aux[i++]; }
+        }
+
+    }
+
+    public static void sort(Comparable a[])
+    {
+
+        int N = a.length;
+        for (int i = 1; i < N; i = i+i ) {
+            for (int j = 0; j < N-i; j+=i+i) {
+
+                merge(a, j, j+i-1, Math.min(N-1, j+i+i-1));
+            }
+        }
+    }
+
+    private  static  void show(Comparable a[])
+    {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(" " + a[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Integer a[]={5,3, 2,1, 4};
+
+        sort(a);
+
+        show(a);
+    }
+}
